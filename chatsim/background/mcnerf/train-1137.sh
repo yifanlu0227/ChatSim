848d4a746770e5dcbe79afe533025c58e1d97acf
yifanlu <yifan_lu@sjtu.edu.cn>
2024-06-12 16:15:51 +0800

    [yfl] fix experiment name

--- chatsim/background/mcnerf/train-1137.sh
+++ chatsim/background/mcnerf/train-1137.sh
@@ -4,4 +4,4 @@ cmake --build build --target main --config RelWithDebInfo -j
 wait
 python scripts/run.py --config-name=wanjinyou_big \
 dataset_name=waymo_multi_view case_name=segment-11379226583756500423_6230_810_6250_810_with_camera_labels \
-exp_name=new_calib_0.3_999*3 dataset.shutter_coefficient=0.3 mode=train_hdr_shutter +work_dir=$(pwd) 
+exp_name=exp_coeff_0.3 dataset.shutter_coefficient=0.3 mode=train_hdr_shutter +work_dir=$(pwd) 
